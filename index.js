2f79cddcc57291fc88c96842a8682def23020d66
davedwight <dpdwight@gmail.com>
2021-04-23 10:06:03 -0600

    completed through topic two request four

--- index.js
+++ index.js
@@ -61,9 +61,9 @@ const zooAnimals = [
   displayNames will be an array of strings, and each string should follow this pattern: "name: {name}, scientific: {scientific name}"
   */
 
-  function animalNames(zooAnimals){
+  function animalNames(arr){
     let displayNames = [];
-    zooAnimals.forEach(item => displayNames.push(`name: ${item.animal_name}, scientific: ${item.scientific_name}`));
+    arr.forEach(item => displayNames.push(`name: ${item.animal_name}, scientific: ${item.scientific_name}`));
     return displayNames;
   }
   console.log(animalNames(zooAnimals));
@@ -76,18 +76,23 @@ const zooAnimals = [
   For example: ['jackal, asiatic', .....]
   */
 
-  function lowerCaseNames(/*Your Code Here*/){
-    /*Your Code Here*/
-  }
-  
+  function lowerCaseNames(zooAnimals){
+    let newArray = zooAnimals.map(function(item){
+        return item.animal_name.toLowerCase();
+    });
+      console.log(newArray);
+      return newArray;
+    }
+    lowerCaseNames(zooAnimals);
   
   /* 🦁🦁🦁 Request 3: .filter() 🦁🦁🦁
   The zoo is concerned about animals with a lower population count. 
   Using lowPopulationAnimals use .filter() to create a new array of objects which contains only the animals with a population of less than 5.
   */
 
-  function lowPopulationAnimals(/*Your Code Here*/){
-    /*Your Code Here*/
+  function lowPopulationAnimals(arr){
+      const lowPop = arr.filter(item => item.population < 5);
+      return lowPop;
   }
   
 
@@ -97,8 +102,9 @@ const zooAnimals = [
   Remember the reduce method takes two arguments: a callback (which itself takes two args - the accumulator and the item), and an initial value for the count.
   */
 
-  function USApop(/*Your Code Here*/){
-    /*Your Code Here*/
+  function USApop(arr){
+    const totalPop = arr.reduce((acc, item) => { return acc + item.population }, 0);
+    return totalPop;
   }
   
   
